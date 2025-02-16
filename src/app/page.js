47d1052eb7f24545d48fb94140eebd2d12551d0b
sajib689 <jibonraja11@gmail.com>
2025-02-16 13:22:09 +0600

    add animation for the youtube section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection";
+import YoutubeSection from "@/components/YoutubeSection";
 
 
 export default function Home() {
   return (
     <div>
       <HeroSection/>
+      <YoutubeSection/>
     </div>
   );
 }
