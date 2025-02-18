16c284948f47a3241427592d8e50e06114579991
sajib689 <jibonraja11@gmail.com>
2025-02-18 11:37:32 +0600

    Add react-slick and slick-carousel for image slider functionality in SliderSection

--- src/components/SliderSection.jsx
+++ src/components/SliderSection.jsx
@@ -1,34 +1,46 @@
 "use client";
 
-import Flicking from "@egjs/react-flicking";
-import "@egjs/react-flicking/dist/flicking.css";
+import React from "react";
+import Slider from "react-slick";
 import Image from "next/image";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const images = [
   "/1.png", "/2.png", "/3.png", "/4.png", "/5.png", "/6.png", "/7.png"
 ];
 
 const SliderSection = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    centerMode: true,
+    centerPadding: "0px",
+    arrows: false, 
+  };
+
   return (
     <div className="w-full flex justify-center items-center p-4">
-      <Flicking
-        circular={true}
-        autoPlay={true}
-        autoPlayInterval={2000}
-        className="w-full"
-      >
+      <Slider {...settings} className="w-full max-w-4xl flex justify-center items-center">
         {images.map((src, index) => (
-          <div key={index} className="mx-2 w-[300px]">
-            <Image
-              src={src}
-              alt={`Slide ${index + 1}`}
-              width={100}
-              height={120}
-              priority
+          <div key={index} className="flex justify-center items-center px-2">
+            <Image 
+              src={src} 
+              alt={`Slide ${index + 1}`} 
+              width={120} 
+              height={120} 
+              priority 
+              className="rounded-lg"
             />
           </div>
         ))}
-      </Flicking>
+      </Slider>
     </div>
   );
 };
